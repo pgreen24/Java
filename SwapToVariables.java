d24422dd4f441dc640316b00b2d9d61f15a1a03d
Rabeya Akter Lima <rabeyaakterlim4@gmail.com>
2025-07-08 23:21:02 +0600

    Delete SwapToVariables.java

--- SwapToVariables.java
+++
@@ -1,8 +0,0 @@
-public class SwapToVariables {
-    public static void main(String[] args) {
-        String x = "water";
-        String y = "color-water";
-        System.out.println("x:" + y);
-        System.out.println("y:" + x);
-    }
-}
