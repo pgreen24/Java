a438b28a0963b6ce6e2d20d7f75fcd4d2d2e94f9
Rabeya Akter Lima <rabeyaakterlim4@gmail.com>
2025-07-08 23:25:02 +0600

    Delete ToStringMethodCars.java

--- ToStringMethodCars.java
+++
@@ -1,9 +0,0 @@
-public class ToStringMethodCars {
-    String name = "farari";
-    String model = "mustang";
-    String color = "black";
-    int year = 2025;
-    public String toString() {
-        return this.name + "\n" + this.model + "\n" + this.color + "\n" + this.year + "\n";
-    }
-}
